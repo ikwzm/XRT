7104cc336e010e3e0755264ff832d811ff605e2c
ikwzm <ichiro_k@ca2.so-net.ne.jp>
2020-03-24 21:07:11 +0900

    [add] dkms-zocl to postinst and prerm

--- src/CMake/dkms-edge.cmake
+++ src/CMake/dkms-edge.cmake
@@ -30,58 +30,71 @@ configure_file (
   @ONLY
   )
 
-SET (XRT_DKMS_DRIVER_SRC_DIR     ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/edge/ )
-SET (XRT_DKMS_DRIVER_INCLUDE_DIR ${XRT_DKMS_DRIVER_SRC_BASE_DIR}/edge/ )
-SET (XRT_DKMS_CORE_DIR ${XRT_DKMS_DRIVER_SRC_BASE_DIR})
-SET (XRT_DKMS_CORE_COMMON_DRV ${XRT_DKMS_CORE_DIR}/common/drv)
+SET (XRT_DKMS_DRIVER_SRC_DIR     ${XRT_DKMS_DRIVER_SRC_BASE_DIR} )
+SET (XRT_DKMS_DRIVER_INCLUDE_DIR ${XRT_DKMS_DRIVER_SRC_BASE_DIR} )
+SET (XRT_DKMS_CORE_DIR           ${XRT_DKMS_DRIVER_SRC_BASE_DIR})
+SET (XRT_DKMS_CORE_COMMON_DRV    ${XRT_DKMS_CORE_DIR}/common/drv)
 
 SET (XRT_DKMS_DRIVER_SRCS
-  drm/zocl/LICENSE
-  drm/zocl/Makefile
-  drm/zocl/sched_exec.c
-  drm/zocl/sched_exec.h
-  drm/zocl/zocl_bo.c
-  drm/zocl/zocl_bo.h
-  drm/zocl/zocl_cu.c
-  drm/zocl/zocl_cu.h
-  drm/zocl/zocl_dma.c
-  drm/zocl/zocl_dma.h
-  drm/zocl/zocl_drv.c
-  drm/zocl/zocl_drv.h
-  drm/zocl/zocl_ert.c
-  drm/zocl/zocl_ert.h
-  drm/zocl/zocl_generic_cu.c
-  drm/zocl/zocl_generic_cu.h
-  drm/zocl/zocl_ioctl.c
-  drm/zocl/zocl_ioctl.h
-  drm/zocl/zocl_mailbox.c
-  drm/zocl/zocl_mailbox.h
-  drm/zocl/zocl_ospi_versal.c
-  drm/zocl/zocl_ospi_versal.h
-  drm/zocl/zocl_ov_sysfs.c
-  drm/zocl/zocl_sk.c
-  drm/zocl/zocl_sk.h
-  drm/zocl/zocl_sysfs.c
-  drm/zocl/zocl_util.h
-  drm/zocl/zocl_xclbin.c
-  drm/zocl/zocl_xclbin.h
+  edge/drm/zocl/LICENSE
+  edge/drm/zocl/Makefile
+  edge/drm/zocl/sched_exec.c
+  edge/drm/zocl/sched_exec.h
+  edge/drm/zocl/zocl_bo.c
+  edge/drm/zocl/zocl_bo.h
+  edge/drm/zocl/zocl_cu.c
+  edge/drm/zocl/zocl_cu.h
+  edge/drm/zocl/zocl_dma.c
+  edge/drm/zocl/zocl_dma.h
+  edge/drm/zocl/zocl_drv.c
+  edge/drm/zocl/zocl_drv.h
+  edge/drm/zocl/zocl_ert.c
+  edge/drm/zocl/zocl_ert.h
+  edge/drm/zocl/zocl_generic_cu.c
+  edge/drm/zocl/zocl_generic_cu.h
+  edge/drm/zocl/zocl_ioctl.c
+  edge/drm/zocl/zocl_ioctl.h
+  edge/drm/zocl/zocl_mailbox.c
+  edge/drm/zocl/zocl_mailbox.h
+  edge/drm/zocl/zocl_ospi_versal.c
+  edge/drm/zocl/zocl_ospi_versal.h
+  edge/drm/zocl/zocl_ov_sysfs.c
+  edge/drm/zocl/zocl_sk.c
+  edge/drm/zocl/zocl_sk.h
+  edge/drm/zocl/zocl_sysfs.c
+  edge/drm/zocl/zocl_util.h
+  edge/drm/zocl/zocl_xclbin.c
+  edge/drm/zocl/zocl_xclbin.h
   )
 
 # includes relative to core/edge/
 SET (XRT_DKMS_DRIVER_INCLUDES
-  include/sk_types.h
-  include/xclhal2_mpsoc.h
-  include/zynq_ioctl.h
-  include/zynq_perfmon_params.h
+  edge/include/sk_types.h
+  edge/include/xclhal2_mpsoc.h
+  edge/include/zynq_ioctl.h
+  edge/include/zynq_perfmon_params.h
   )
 
 # includes relative to core
 SET (XRT_DKMS_CORE_INCLUDES
   include/ert.h
-  include/xclfeatures.h
+  include/stream.h
+  include/types.h
+  include/xbar_sys_parameters.h
+  include/xcl_api_macros.h
+  include/xcl_app_debug.h
+  include/xcl_axi_checker_codes.h
   include/xclbin.h
   include/xclerr.h
+  include/xclfeatures.h
+  include/xclhal2.h
+  include/xclhal2_mem.h
+  include/xcl_macros.h
+  include/xclperf.h
+  include/xcl_perfmon_parameters.h
+  include/xrt.h
   include/xrt_mem.h
+  include/xstream.h
   )
 
 SET (XRT_DKMS_COMMON_XRT_DRV
